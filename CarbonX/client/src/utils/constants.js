b2859f42a20d0cc7e7b343d5fbd9e51a5bc8b4d2
achicago <achigonye@gmail.com>
2022-10-22 05:23:21 -0700

    Created constant.js

---
+++ CarbonX/client/src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from "./Transactions.json";
+
+// export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x30C06ac9FCAfD569e62bc40e5EB39a32495BbE5C";
+
+export const contractABI = abi.abi;
